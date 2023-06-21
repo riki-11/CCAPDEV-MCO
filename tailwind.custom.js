bb9be422cb82381c55dc0786a6845b0a23868d88
riki-11 <lejanoenrique@gmail.com>
2023-06-21 21:23:39 +0800

    Initial merging of all files.

---
+++ tailwind.custom.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+      extend: {
+      fontFamily: {
+          'primary': ['Montserrat', 'sans-serif']
+      },
+      colors:  {
+          'secondary': '#F5CB5C',
+          'primarybg':'#202020'
+      }
+      },
+  },
+}
