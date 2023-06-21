fc89e2bc8781a22b001936310a86c747b44db4f5
PJLeonida <105859662+PJLeonida@users.noreply.github.com>
2023-06-21 20:38:52 +0800

    Patsbb module
    Kulang ung node.js file

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.{html,js}',
+            './src/editprofile.html',
+            './src/viewprofile.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'primary': ['Montserrat', 'sans-serif']
+      },
+      colors:  {
+        'secondary': '#F5CB5C',
+        'primarybg':'#202020'
+      }
+    },
+  },
+  plugins: [],
+}
+
